a835973a82e8fd5c64225cecc2df1dc1498b1f37
Berend Zwartsenberg <berend@zwartsenberg.nl>
2016-05-22 22:02:53 -0700

    Add armadillo library to include libraries

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-# Cython compile instructions
-
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-# Use python setup.py build --inplace
-# to compile
-
-setup(
-  name = "ConnectedSystem",
-  ext_modules = cythonize('*.pyx'),
-)
+setup(ext_modules = cythonize(Extension(
+            "consys",                                # the extension name
+            sources=["consys.pyx", "ConnectedSystem.cxx", ], # the Cython source and
+                                                  # additional C++ source files
+            library_dirs = ['/usr/lib', '/usr/local/lib', '/Users/berend/Documents/Coding/armadillo-6.700.7/include'],
+            libraries=["armadillo"],
+            language="c++",                        # generate and compile C++ code
+      )))
